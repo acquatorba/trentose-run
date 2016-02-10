834645b07eb4ba400b875e7925b972131a463887
Giulio Maria Facin <giulio.maria@gmail.com>
2016-02-10 11:31:44 +0100

    soluzione1

--- dev-code/app.js
+++ dev-code/app.js
@@ -1,13 +1,63 @@
-/* your code should go here */
 
-$(document).ready(function(){
-  
-  
-});
+var Controller ={
+    init: function(){
+        View.show(data);
+        View.init();
+    },
+    GetAllRunner: function(){
+        return data;
+    },
+    SelectRunner: function(cat){
+        var sel = [];
+        if(cat=="all"){
+            return data;
+        }
+        for(var i=0;i<data.length; i++){
+            if(data[i].category==cat){
+                sel.push(data[i]);
+            }
+        }
+        return sel;
+    }
+}
 
-
-
-
+var View ={
+    init: function(){
+        this.buttonstart();
+    },
+    show: function(Runners){
+        $("#top-selfiers").html("");
+        for(var i=0; i<Runners.length;i++){
+            console.log(i);
+            $("#top-selfiers").append(
+                "<li>"+
+                "<div class='selfie'>"+
+                    "<img src='"+Runners[i].pic+"'>"+         
+                "</div>"+
+                "<div class='stats'>"+
+                    "<h2>"+Runners[i].name+" <small>"+Runners[i].category+"</small></h2>"+
+                    "<strong>Time</strong>  "+Runners[i].time+""+ 
+                    "<strong>Selfies</strong> "+Runners[i].sefies+""+  
+                "</div> "+
+                "<div class='badge'>"+
+                   " #"+(i+1)+""+
+                "</div>"+
+                "</li> "
+            );
+        }
+    },
+    buttonstart: function(){
+        $("#btn-filter").click(function() {
+            var run = [];
+            console.log("pera");
+            var e = $("#sel").val();
+            run=Controller.SelectRunner(e);
+            View.show(run);
+            console.log(e);
+        })
+    }
+}
+Controller.init();
 
 
 
--- test-code/spec/app.spec.js
+++ test-code/spec/app.spec.js
@@ -1,7 +1,35 @@
-describe("App", function() {
+describe("testo funzione coach.start", function() {
     
-  it("should..", function() { 
-      pending();
+  it("numero calorie da bruciare f", function() { 
+      expect(Coach.start("craz")).not.toBe(true);
+  });
+    
+  it("numero calorie da bruciare e", function() { 
+      expect(Coach.start("easy")).toBe(true);
   });  
+  
+  it("numero calorie da bruciare n", function() { 
+      expect(Coach.start("normal")).toBe(true);
+  }); 
+
+  it("numero calorie da bruciare c", function() { 
+      expect(Coach.start("crazy")).toBe(true);
+  }); 
+  
+   
+ 
+});
+
+describe("Coach.Track", function() {
+    
+  it("walk 50km", function() { 
+      Coach.start("crazy");
+      expect(Coach.track("walk",50)).toEqual(2500);
+  });  
+    
+  it("walk 50km", function() { 
+      Coach.start("crazy");
+      expect(Coach.track("wal",50)).toBe(null);
+  }); 
  
 });
